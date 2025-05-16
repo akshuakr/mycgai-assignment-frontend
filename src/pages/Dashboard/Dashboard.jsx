1ad75d8b46b296bf46d273e33068177cd9319d1e
akshu <in.akr.2000@gmail.com>
2025-05-17 03:39:14 +0530

    Generate reply

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,6 +10,8 @@ const Dashboard = () => {
         <div className={styles.subContainer}>
           <div className={styles.left}>
             <UploadComponent />
+            <GenerateButton />
+            <PreviousNoticesComponent />
           </div>
           <div className={styles.right}>Hi</div>
         </div>
@@ -28,12 +30,7 @@ const UploadComponent = () => {
         <div className={styles.typeOneDesc}>Drag and drop your tax notice here</div>
         <div className={styles.typeTwoDesc}>Supported format: PDF</div>
         <div>
-          <input
-            type="file"
-            id="file-input"
-            accept="application/pdf"
-            style={{ display: "none" }}
-          />
+          <input type="file" id="file-input" accept="application/pdf" style={{ display: "none" }} />
           <label htmlFor="file-input">
             <div className={styles.uploadButton}>
               <UploadDocumentIconPurple />
@@ -43,6 +40,168 @@ const UploadComponent = () => {
         </div>
       </div>
     </>
+  );
+};
+
+const PreviousNoticesComponent = () => {
+  return (
+    <>
+      <h2 style={{ marginTop: "20px" }} className={styles.typeOneHeading}>
+        Previous Notices
+      </h2>
+      <div className={styles.previousNoticesContainer}>
+        <ul className={styles.unorderdList}>
+          <li className={styles.singleList}>
+            <div className={styles.listLeft}>
+              <DocumentIconPurpule />
+              <div className={styles.listCenter}>
+                <div className={styles.listOriginalName}>akshansh_resume1.pdf</div>
+                <div className={styles.listDate}>May 10, 2025</div>
+              </div>
+            </div>
+            <div className={styles.listStatusDone}>
+              <CheckIcon />
+              Reply Ready
+            </div>
+          </li>
+          <li className={styles.singleList}>
+            <div className={styles.listLeft}>
+              <DocumentIconPurpule />
+              <div className={styles.listCenter}>
+                <div className={styles.listOriginalName}>akshansh_resume2.pdf</div>
+                <div className={styles.listDate}>May 11, 2025</div>
+              </div>
+            </div>
+            <div className={styles.listStatusProcessing}>
+              <ClockIcon />
+              Processing...
+            </div>
+          </li>
+          <li className={styles.singleList}>
+            <div className={styles.listLeft}>
+              <DocumentIconPurpule />
+              <div className={styles.listCenter}>
+                <div className={styles.listOriginalName}>akshansh_resume3.pdf</div>
+                <div className={styles.listDate}>May 12, 2025</div>
+              </div>
+            </div>
+            <div className={styles.listStatusDone}>
+              <CheckIcon />
+              Reply Ready
+            </div>
+          </li>
+          <li className={styles.singleList}>
+            <div className={styles.listLeft}>
+              <DocumentIconPurpule />
+              <div className={styles.listCenter}>
+                <div className={styles.listOriginalName}>akshansh_resume4.pdf</div>
+                <div className={styles.listDate}>May 13, 2025</div>
+              </div>
+            </div>
+            <div className={styles.listStatusProcessing}>
+              <ClockIcon />
+              Processing...
+            </div>
+          </li>
+          <li className={styles.singleList}>
+            <div className={styles.listLeft}>
+              <DocumentIconPurpule />
+              <div className={styles.listCenter}>
+                <div className={styles.listOriginalName}>akshansh_resume5.pdf</div>
+                <div className={styles.listDate}>May 14, 2025</div>
+              </div>
+            </div>
+            <div className={styles.listStatusDone}>
+              <CheckIcon />
+              Reply Ready
+            </div>
+          </li>
+          <li className={styles.singleList}>
+            <div className={styles.listLeft}>
+              <DocumentIconPurpule />
+              <div className={styles.listCenter}>
+                <div className={styles.listOriginalName}>akshansh_resume6.pdf</div>
+                <div className={styles.listDate}>May 15, 2025</div>
+              </div>
+            </div>
+            <div className={styles.listStatusProcessing}>
+              <ClockIcon />
+              Processing...
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+const GenerateButton = () => {
+  return (
+    <div className={styles.generateButtonContainer}>
+      <div className={styles.generateButtonContainerLeft}>
+        <div className={styles.generateButtonContainerCheckMark}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="23"
+            height="23"
+            fill="#41ba6f"
+            viewBox="0 0 16 16"
+          >
+            <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0" />
+            <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708" />
+          </svg>
+        </div>
+        <div className={styles.listOriginalName}>one_two.pdf</div>
+      </div>
+      <div className={styles.generateButtonContainerButton}>Generate Reply</div>
+    </div>
+  );
+};
+
+const DocumentIconPurpule = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="#634aff"
+      viewBox="0 0 16 16"
+    >
+      <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+      <path d="M4.603 14.087a.8.8 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.7 7.7 0 0 1 1.482-.645 20 20 0 0 0 1.062-2.227 7.3 7.3 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a11 11 0 0 0 .98 1.686 5.8 5.8 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.86.86 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.7 5.7 0 0 1-.911-.95 11.7 11.7 0 0 0-1.997.406 11.3 11.3 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.8.8 0 0 1-.58.029m1.379-1.901q-.25.115-.459.238c-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361q.016.032.026.044l.035-.012c.137-.056.355-.235.635-.572a8 8 0 0 0 .45-.606m1.64-1.33a13 13 0 0 1 1.01-.193 12 12 0 0 1-.51-.858 21 21 0 0 1-.5 1.05zm2.446.45q.226.245.435.41c.24.19.407.253.498.256a.1.1 0 0 0 .07-.015.3.3 0 0 0 .094-.125.44.44 0 0 0 .059-.2.1.1 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a4 4 0 0 0-.612-.053zM8.078 7.8a7 7 0 0 0 .2-.828q.046-.282.038-.465a.6.6 0 0 0-.032-.198.5.5 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822q.036.167.09.346z" />
+    </svg>
+  );
+};
+
+const CheckIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-check-circle"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+      <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
+    </svg>
+  );
+};
+
+const ClockIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-clock"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+    </svg>
   );
 };
 
